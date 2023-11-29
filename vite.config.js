fa3035950d3a893cae710f3cbe1d2f238f802346
Max Kotyk <mxm.kotyk@gmail.com>
2023-11-29 18:18:48 +0100

    Initial setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react(), svgr()],
+    resolve: {
+      alias: {
+        src: "/src",
+        components: "/src/components",
+        pages: "/src/pages",
+        assets: "/src/assets",
+      },
+    },
+    base: command === "build" ? "/car-rental/" : "/",
+  };
+});
