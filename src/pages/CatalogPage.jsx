1e524539a6d7d63a0789104a8e0c598ed1d0b356
Max Kotyk <mxm.kotyk@gmail.com>
2023-11-30 17:09:11 +0100

    SImple fetched data render test

--- src/pages/CatalogPage.jsx
+++ src/pages/CatalogPage.jsx
@@ -1,5 +1,32 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAdverts } from "../redux/thunks";
+
 const CatalogPage = () => {
-  return <div>Catalog</div>;
+  const dispatch = useDispatch();
+  const adverts = useSelector((state) => state.adverts.adverts);
+
+  console.log(adverts);
+
+  useEffect(() => {
+    dispatch(fetchAdverts());
+  }, [dispatch]);
+
+  return (
+    <>
+      <h1>Catalog</h1>
+      <ul>
+        {adverts.map((advert) => (
+          <li key={advert.id}>
+            <img src={advert.img} alt={`${advert.make} ${advert.model}`} />
+            <p>
+              {advert.id} {advert.make} {advert.model} {advert.year}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default CatalogPage;
