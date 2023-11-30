3f7fbff738ce98f0813f08d04bc0fe06b162b22e
Max Kotyk <mxm.kotyk@gmail.com>
2023-11-30 21:29:38 +0100

    Added Load more button

--- src/pages/CatalogPage.jsx
+++ src/pages/CatalogPage.jsx
@@ -1,30 +1,29 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchAdverts } from "../redux/thunks";
+import { useState } from "react";
+import { useGetAllAdvertsQuery } from "../redux/advertsApi";
 
 const CatalogPage = () => {
-  const dispatch = useDispatch();
-  const adverts = useSelector((state) => state.adverts.adverts);
+  const [page, setPage] = useState(1);
+  const { data } = useGetAllAdvertsQuery(page);
 
-  console.log(adverts);
-
-  useEffect(() => {
-    dispatch(fetchAdverts());
-  }, [dispatch]);
+  console.log(data);
 
   return (
     <>
       <h1>Catalog</h1>
       <ul>
-        {adverts.map((advert) => (
-          <li key={advert.id}>
-            <img src={advert.img} alt={`${advert.make} ${advert.model}`} />
-            <p>
-              {advert.id} {advert.make} {advert.model} {advert.year}
-            </p>
-          </li>
-        ))}
+        {data &&
+          data.map((advert) => (
+            <li key={advert.id}>
+              <img src={advert.img} alt={`${advert.make} ${advert.model}`} />
+              <p>
+                {advert.id} {advert.make} {advert.model} {advert.year}
+              </p>
+            </li>
+          ))}
       </ul>
+      <button type="button" onClick={() => setPage((prev) => prev + 1)}>
+        Load More
+      </button>
     </>
   );
 };
