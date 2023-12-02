3d376c63f4f3cd592a4b693c0609d492ec932329
Max Kotyk <mxm.kotyk@gmail.com>
2023-12-02 13:02:04 +0100

    Fixed Container styles

--- src/components/Container/Container.styled.jsx
+++ src/components/Container/Container.styled.jsx
@@ -1,7 +1,7 @@
 import styled from "@emotion/styled";
 
 export const StyledContainer = styled.div`
-  max-width: 1400px;
-  padding-inline: 128px;
+  max-width: 1440px;
+  padding: 150px 128px;
   margin-inline: auto;
 `;
