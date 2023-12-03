89d9b0dda88c9464450189cac180eab0adb3c7d1
Max Kotyk <mxm.kotyk@gmail.com>
2023-12-03 16:05:06 +0100

    Added FavoritesPage

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,5 +1,28 @@
+import { useSelector } from "react-redux";
+import Container from "../components/Container/Container";
+import Section from "../components/Section/Section";
+import Card from "../components/cards/Card/Card";
+import CardGrid from "../components/cards/CardGrid/CardGrid";
+import PageTitle from "../components/PageTitle/PageTitle";
+
 const FavoritesPage = () => {
-  return <div>Favorites</div>;
+  const favorites = useSelector((store) => store.favorites.favorites) ?? [];
+
+  return (
+    <>
+      <Section>
+        <Container>
+          <PageTitle>Favorites</PageTitle>
+          <CardGrid>
+            {favorites &&
+              favorites.map((advert) => (
+                <Card advert={advert} key={advert.id} />
+              ))}
+          </CardGrid>
+        </Container>
+      </Section>
+    </>
+  );
 };
 
 export default FavoritesPage;
